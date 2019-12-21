8d3476827d1fe75b58b7ced7bef0a9e53b6d7224
kazma onomura <kzonomu@gmail.com>
2019-12-21 16:48:27 +0900

    6.9まで完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = 'ユーザーを新規登録しました！'
+      redirect_to user_url @user
+    else
+      render :new
+    end
+  end
+end
